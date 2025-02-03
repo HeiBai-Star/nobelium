fffef243bd86784e178ed5baf09acf03c902af57
HeiBai-Star <66226033+HeiBai-Star@users.noreply.github.com>
2025-02-03 17:06:43 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -23,7 +23,7 @@ const BLOG = {
     googleSiteVerification: '' // Remove the value or replace it with your own google site verification code
   },
   // notionPageId: process.env.NOTION_PAGE_ID, // DO NOT CHANGE THIS！！！
-  notionPageId: '191318984a5240579df45c1fc7ca371b',
+  notionPageId: '18f0ea35798f8067813ff44fbb58a06d',
   notionAccessToken: process.env.NOTION_ACCESS_TOKEN, // Useful if you prefer not to make your database public
   analytics: {
     provider: 'ga', // Currently we support Google Analytics and Ackee, please fill with 'ga' or 'ackee', leave it empty to disable it.
